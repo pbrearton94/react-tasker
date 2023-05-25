151dc5242360c973c063487e3b2868443b904c38
pbrearton94 <pbrearton@gmail.com>
2023-05-25 13:10:13 -0400

    Completed basic functionality of the todo list, add, remove and updates are working.

--- src/components/task/task.component.jsx
+++ src/components/task/task.component.jsx
@@ -1,23 +1,64 @@
 import React, { useEffect, useState } from "react";
 import testApp from "../../services/firebase.config";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
 import "./task.styles.scss";
 
 const Task = () => {
   const [tasks, setTasks] = useState([]);
+  const [createTask, setCreateTask] = useState([]);
   const db = getFirestore();
   const collectionRef = collection(db, "tasks");
 
   useEffect(() => {
-    const getTasks = async () => { 
-      await getDocs(collectionRef).then((task) => {
-        let tasksData = task.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-        setTasks(tasksData);
-      });
+    const getTasks = async () => {
+      await getDocs(collectionRef)
+        .then((task) => {
+          let tasksData = task.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }));
+          setTasks(tasksData);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     };
-
     getTasks();
   }, []);
+
+  //Add Task Handler
+  const submitTask = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addDoc(collectionRef, {
+        task: createTask,
+        isChecked: false,
+      });
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //Delete Task Handler
+  const deleteTask = async (id) => {
+    try {
+      window.confirm("Are you sure you want to delete this task?");
+      const documentRef = doc(db, "tasks", id);
+      await deleteDoc(documentRef);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -25,6 +66,14 @@ const Task = () => {
         <div className="row col-md-12">
           <div className="card card-black">
             <div className="card-body">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#addTask"
+              >
+                Add Task
+              </button>
               {tasks.map(({ task, id }) => (
                 <div className="todo-list" key={id}>
                   <div className="todo-item">
@@ -45,6 +94,7 @@ const Task = () => {
                         Edit
                       </button>
                       <button
+                        onClick={() => deleteTask(id)}
                         type="button"
                         className="btn btn-danger float-end mx-3"
                       >
@@ -56,6 +106,51 @@ const Task = () => {
               ))}
             </div>
           </div>
+        </div>
+      </div>
+      <div
+        className="modal fade"
+        id="addTask"
+        tabIndex="-1"
+        aria-labelledby="addTaskLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <form onSubmit={submitTask} className="d-flex">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="addTaskLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <input
+                  onChange={(event) => setCreateTask(event.target.value)}
+                  type="text"
+                  className="form-control"
+                  placeholder="Add a task"
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="submit" className="btn btn-primary">
+                  Add Task
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </>
---
+++ src/services/firebase.config.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app';
+import {getFireStore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAbZg34o38PMkyAG6qySw1Uq35BbgDW_o",  
+    authDomain: "tasker-e23ad.firebaseapp.com",
+    projectId: "tasker-e23ad",  
+    storageBucket: "tasker-e23ad.appspot.com",
+    messagingSenderId: "1045483858911",
+    appId: "1:1045483858911:web:bc5509c75b62503e22f26a"
+  };
+  
+  const testApp = initializeApp(firebaseConfig);
+
+  export default testApp;
